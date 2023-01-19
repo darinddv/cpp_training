e4a1c0aea4cf2106f6e27642450405cb09f5a679
darinddv <47702590+darinddv@users.noreply.github.com>
2023-01-19 03:52:14 -0500

    do ch2 assignment and reorganize folders with chapters

---
+++ gaddis_cpp/exercises/cpp/ch2/ch2ex1_sum.cpp
@@ -0,0 +1,15 @@
+/*
+1. Sum of Two Numbers
+Write a program that stores the integers 50 and 100 in
+variables and stores the sum of these two in a variable
+named total. Display the total on the screen.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1 {50}, num2 {100}, total {};
+    cout << "The sume of num1 and num2 is: " << total << endl;
+    return 0;
+}
